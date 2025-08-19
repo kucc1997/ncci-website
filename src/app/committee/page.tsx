d848bdf09f6ed797d62d845baaa40af0b674216b
Abhiyan Dhakal <itsabhiyandhakal@gmail.com>
2025-08-19 11:16:00 +0545

    feat(committee): update committee designation

--- src/app/committee/page.tsx
+++ src/app/committee/page.tsx
@@ -189,7 +189,8 @@ export default function CommitteePage() {
 							</div>
 							<h3 className="text-xl font-semibold">Dr. Manoj Shakya</h3>
 							<p className="text-gray-600">Assistant Professor</p>
-							<p className="text-gray-600 max-w-60">Department of Computer Science and Engineering</p>
+							<p className="text-gray-600">Acting Head</p>
+							<p className="text-gray-600 max-w-60">Department of Artificial Intelligence</p>
 							<p className="text-gray-600">Kathmandu University</p>
 						</div>
 						<div className="text-center sm:max-w-xs">
@@ -278,8 +279,8 @@ export default function CommitteePage() {
 								/>
 							</div>
 							<h3 className="text-xl font-semibold">Amrit Dahal</h3>
-							<p className="text-gray-600">Lecturer</p>
-							<p className="text-gray-600 max-w-60">Department of Computer Science and Engineering</p>
+							<p className="text-gray-600">Assistant Professor</p>
+							<p className="text-gray-600 max-w-60">Department of Artificial Intelligence</p>
 							<p className="text-gray-600">Kathmandu University</p>
 						</div>
 						<div className="text-center sm:max-w-xs">
@@ -425,12 +426,12 @@ export default function CommitteePage() {
 										affiliation: "KU, DoCSE",
 										image: "/oc/ShreejanKarmacharya.webp"
 									},
-                  {
-                    name: "Shriharsh Sharma Acharya",
-                    position: "Member",
-                    affiliation: "KU, DoCSE",
-                    image: "/oc/Shriharsh.webp"
-                  },
+									{
+										name: "Shriharsh Sharma Acharya",
+										position: "Member",
+										affiliation: "KU, DoCSE",
+										image: "/oc/Shriharsh.webp"
+									},
 								].map((member, index) => (
 									<div key={index} className="text-center max-w-xs">
 										<div className="relative w-36 h-36 mx-auto mb-4 rounded-full overflow-hidden border-4 border-blue-100">
